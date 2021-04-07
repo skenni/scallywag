3f0a15954f91beade4fcbbed84da5cb1eb978221
Stephen Kenny <stephen.kenny@ericsson.com>
2021-04-07 15:35:20 +0100

    test change GOODBYE

--- go/hello-world.go
+++ go/hello-world.go
@@ -14,7 +14,7 @@ func main() {
     })
 
     http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "goodbye")
+        fmt.Fprintf(w, "GOODBYE")
     })
 
     http.HandleFunc("/MARCO", func(w http.ResponseWriter, r *http.Request){
